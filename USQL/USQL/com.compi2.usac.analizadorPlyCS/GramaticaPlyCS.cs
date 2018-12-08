229c8499012d505a3766521a29305c0477bdbc0c
Elmer <alayelmer1993@gmail.com>
2018-12-08 08:28:02 -0600

    Gramática inicial del lenguaje PlyCS de parte del servidor de bases de datos

---
+++ USQL/USQL/com.compi2.usac.analizadorPlyCS/GramaticaPlyCS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Irony.Ast;
+using Irony.Parsing;
+
+namespace USQL.com.compi2.usac.analizadorPlyCS
+{
+    class GramaticaPlyCS : Grammar
+    {
+        public GramaticaPlyCS()
+            : base(caseSensitive: false)
+        {
+            #region Expresiones Regulares
+            RegexBasedTerminal numero = new RegexBasedTerminal("numero", "[0-9]+");
+            IdentifierTerminal id = new IdentifierTerminal("id");
+            StringLiteral cadenaSinComillas = TerminalFactory.CreateCSharpString("cadenaSinComillas");
+            StringLiteral cadena = new StringLiteral("cadena", "\"");
+            #endregion
+
+            #region Terminales
+            #endregion
+
+            #region No Terminales
+            #endregion
+
+            #region Gramatica
+            #endregion
+
+            #region Preferencias
+            #endregion
+        }
+    }
+}
